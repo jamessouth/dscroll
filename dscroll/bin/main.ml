f24f9e7c617c964f262731e6c54c52ceacaf972b
jamessouth <jamesdanielsouth@gmail.com>
2026-05-06 00:20:37 -0500

    add dir

--- dscroll/bin/main.ml
+++ dscroll/bin/main.ml
@@ -1,14 +1,25 @@
 open Core
 
+let direction =
+  Command.Arg_type.create (fun dir ->
+      match dir with
+      | "l" | "r" | "b" -> dir
+      | _ -> failwith "invalid direction")
+
 let command =
   Command.basic ~summary:"Generate an MD5 hash of the input data"
     ~readme:(fun () -> "More detailed information")
-    (let%map_open.Command text = anon ("text" %: string)
+    (let%map_open.Command text =
+       anon (non_empty_sequence_as_list ("text" %: string))
      and length =
        flag_optional_with_default_doc "-length" int
          (fun x -> Int.sexp_of_t x)
          ~default:15 ~doc:"int width"
+     and direction =
+       flag_optional_with_default_doc "-direction" direction
+         (fun x -> String.sexp_of_t x)
+         ~default:"l" ~doc:"direction"
      in
-     fun () -> Dscroll.run text length)
+     fun () -> Dscroll.run text length direction)
 
 let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
--- dscroll/lib/dscroll.ml
+++ dscroll/lib/dscroll.ml
@@ -1,5 +1,6 @@
 open Core
 
-let run text length =
-  text |> print_endline;
-  length |> string_of_int |> print_endline
+let run text length direction =
+  List.iter text ~f:(fun word -> printf "%s " word);
+  length |> string_of_int |> print_endline;
+  direction |> print_endline
