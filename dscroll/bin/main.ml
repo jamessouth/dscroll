55e57e1ba2c11a246fadc1c7b90eb943e0ddfe17
jamessouth <jamesdanielsouth@gmail.com>
2026-05-16 01:04:50 -0500

    args

--- dscroll/bin/main.ml
+++ dscroll/bin/main.ml
@@ -1,5 +1,8 @@
 open Core
 
+(* The only zeroless pandigital number where the first n digits are divisible by n, used as 'infinity' *)
+let quasi_inf = 381_654_729
+
 let direction =
   Command.Arg_type.create (fun dir ->
       match dir with
@@ -7,67 +10,70 @@ let direction =
       | _ ->
           failwith "invalid direction - must be one of left, right, or bounce")
 
-let nonnegint ~msg ~min num =
+let nonnegint ~min num =
   match num |> int_of_string_opt with
-  | Some n -> (
-      match n with
-      | n when n >= min -> n
-      | _ ->
-          failwith
-            ("invalid " ^ msg ^ " - must be an int >= " ^ string_of_int min))
+  | Some n -> Int.max min n
   | None -> failwith "not an int"
 
-let cycles = Command.Arg_type.create (nonnegint ~msg:"cycles" ~min:0)
+let cyc = Command.Arg_type.create (nonnegint ~min:0)
+let ecl = Command.Arg_type.create (nonnegint ~min:1)
+let spd = Command.Arg_type.create (nonnegint ~min:1)
+let wid = Command.Arg_type.create (nonnegint ~min:1)
 
-let ecl =
-  Command.Arg_type.create (nonnegint ~msg:"minimum endcap length" ~min:1)
+let flags : Dscroll.cliflags Command.Param.t =
+  let%map_open.Command cycles =
+    flag_optional_with_default_doc "--cycles" ~aliases:[ "-c" ] cyc
+      (fun x -> Int.sexp_of_t x)
+      ~default:quasi_inf ~doc:"int number of scroll cycles"
+  and direction =
+    flag_optional_with_default_doc "--direction" ~aliases:[ "-d" ] direction
+      (fun x -> String.sexp_of_t x)
+      ~default:"left" ~doc:"string left, right, or bounce"
+  and endcap_char =
+    flag_optional_with_default_doc "--endcap-char" ~aliases:[ "-ecc" ] char
+      (fun x -> Char.sexp_of_t x)
+      ~default:' ' ~doc:"char pad between end and start of TEXT"
+  and endcap_len =
+    flag_optional_with_default_doc "--endcap-len" ~aliases:[ "-ecl" ] ecl
+      (fun x -> Int.sexp_of_t x)
+      ~default:1 ~doc:"int minimum length of endcap"
+  and no_newline =
+    flag "--no-newline" ~aliases:[ "-nnl" ] no_arg
+      ~doc:" do not add newline to output"
+  and prefix =
+    flag_optional_with_default_doc "--prefix" ~aliases:[ "-p" ] string
+      (fun x -> String.sexp_of_t x)
+      ~default:"" ~doc:"string prefix at left of display"
+  and speed =
+    flag_optional_with_default_doc "--speed" ~aliases:[ "-sp" ] spd
+      (fun x -> Int.sexp_of_t x)
+      ~default:300 ~doc:"int sleep in ms per scroll of TEXT"
+  and suffix =
+    flag_optional_with_default_doc "--suffix" ~aliases:[ "-s" ] string
+      (fun x -> String.sexp_of_t x)
+      ~default:"" ~doc:"string suffix at right of display"
+  and width =
+    flag_optional_with_default_doc "--width" ~aliases:[ "-w" ] wid
+      (fun x -> Int.sexp_of_t x)
+      ~default:15 ~doc:"int display width"
+  in
+  {
+    Dscroll.cycles;
+    direction;
+    endcap_char;
+    endcap_len;
+    no_newline;
+    prefix;
+    speed;
+    suffix;
+    width;
+  }
 
-let speed = Command.Arg_type.create (nonnegint ~msg:"speed" ~min:1)
-let width = Command.Arg_type.create (nonnegint ~msg:"width" ~min:1)
-
-let command =
-  Command.basic ~summary:"Generate an MD5 hash of the input data"
-    ~readme:(fun () -> "More detailed information")
-    (let%map_open.Command text =
-       anon (non_empty_sequence_as_list ("text" %: string))
-     and cycles =
-       flag_optional_with_default_doc "--cycles" ~aliases:[ "-c" ] cycles
-         (fun x -> Int.sexp_of_t x)
-         ~default:Int.max_value ~doc:"int # of scroll cycles"
-     and direction =
-       flag_optional_with_default_doc "--direction" ~aliases:[ "-d" ] direction
-         (fun x -> String.sexp_of_t x)
-         ~default:"left" ~doc:"string left, right, or bounce"
-     and endcap_char =
-       flag_optional_with_default_doc "--endcap-char" ~aliases:[ "-ec" ] char
-         (fun x -> Char.sexp_of_t x)
-         ~default:' ' ~doc:"char pad between end and start of TEXT"
-     and endcap_len =
-       flag_optional_with_default_doc "--endcap-len" ~aliases:[ "-ecl" ] ecl
-         (fun x -> Int.sexp_of_t x)
-         ~default:1 ~doc:"int minimum length of endcap"
-     and no_newline =
-       flag "--no-newline" ~aliases:[ "-nnl" ] no_arg
-         ~doc:" do not add newline to output"
-     and prefix =
-       flag_optional_with_default_doc "--prefix" ~aliases:[ "-p" ] string
-         (fun x -> String.sexp_of_t x)
-         ~default:"" ~doc:"string prefix at left of display"
-     and sleep =
-       flag_optional_with_default_doc "--speed" ~aliases:[ "-sp" ] speed
-         (fun x -> Int.sexp_of_t x)
-         ~default:300 ~doc:"int sleep in ms per scroll of TEXT"
-     and suffix =
-       flag_optional_with_default_doc "--suffix" ~aliases:[ "-s" ] string
-         (fun x -> String.sexp_of_t x)
-         ~default:"" ~doc:"string suffix at right of display"
-     and width =
-       flag_optional_with_default_doc "--width" ~aliases:[ "-w" ] width
-         (fun x -> Int.sexp_of_t x)
-         ~default:15 ~doc:"int display width"
-     in
-     fun () ->
-       Dscroll.run text width direction prefix suffix endcap_char endcap_len
-         sleep no_newline)
-
-let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
+let () =
+  Command_unix.run ~version:"1.0" ~build_info:"RWO"
+    (Command.basic ~summary:"Generate an MD5 hash of the input data"
+       ~readme:(fun () -> "More detailed information")
+       (let%map_open.Command text =
+          anon (non_empty_sequence_as_list ("text" %: string))
+        and flags in
+        fun () -> Dscroll.run text flags))
--- dscroll/lib/dscroll.ml
+++ dscroll/lib/dscroll.ml
@@ -1,5 +1,17 @@
 open Core
 
+type cliflags = {
+  cycles : int;
+  direction : string;
+  endcap_char : char;
+  endcap_len : int;
+  no_newline : Base.bool;
+  prefix : string;
+  speed : int;
+  suffix : string;
+  width : int;
+}
+
 let rec loop text delay width cnt =
   match cnt = 0 with
   | true -> exit 0
@@ -13,22 +25,23 @@ let rec loop text delay width cnt =
         (concat [ slice text 1 (length text); slice wrds 0 1 ])
         delay width (cnt - 1)
 
-let run text width direction prefix suffix endcap_char endcap_len sleep
-    no_newline =
+let run text flags =
   let words = text |> String.concat ~sep:" " in
   let endcap =
     String.make
-      (Int.max (width - String.length words) (Int.min (width - 1) endcap_len))
-      endcap_char
+      (Int.max
+         (flags.width - String.length words)
+         (Int.min (flags.width - 1) flags.endcap_len))
+      flags.endcap_char
   in
   print_endline (words ^ endcap);
 
   let delay =
-    [ sleep |> string_of_int; "ms" ]
+    [ flags.speed |> string_of_int; "ms" ]
     |> String.concat |> Time_float_unix.Span.of_string
   in
 
-  loop (words ^ endcap) delay width 500
+  loop (words ^ endcap) delay flags.width 500
 
 (* let run text width direction prefix suffix endcap_char endcap_len sleep no_newline =
     List.iter text ~f:(fun word -> printf "%s " word);
